73d6a25c64afbb1ca91e470c2c6ac4ec030627a5
MiriamWisky <miriamwisky11@gmail.com>
2024-10-09 20:31:18 +0300

    Add Express server to serve client build

---
+++ client/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// משרת את הקבצים הסטטיים מתוך תיקיית build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// כל בקשה אחרת מנותבת ל-index.html כדי לאפשר שימוש ב-React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
